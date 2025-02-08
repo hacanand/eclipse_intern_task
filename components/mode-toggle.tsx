7d5b1eb1d1f040d4de3df3c52221092e6dfc9ddd
anand <anandpromax@gmail.com>
2025-02-08 15:01:21 +0530

    Fix import order in mode-toggle component

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
